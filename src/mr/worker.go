12c32c389798f5d630c5430fa06984b0fd84f9db
GGGalaxy <33902518+wwShuang@users.noreply.github.com>
2021-09-15 20:54:59 +0800

    Hints One started: change documents to test

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -27,14 +27,17 @@ func ihash(key string) int {
 
 //
 // main/mrworker.go calls this function.
-//
+// One way to get started is to modify mr/worker.go's Worker() to send an RPC to the coordinator asking for a task. 
+//Then modify the coordinator to respond with the file name of an as-yet-unstarted map task. 
+//Then modify the worker to read that file and call the application Map function, as in mrsequential.go.  
+
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
 	// Your worker implementation here.
 
 	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
+	CallExample()
 
 }
 
@@ -65,7 +68,9 @@ func CallExample() {
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
+// When the job is completely finished, the worker processes should exit. A simple way to implement this is to use the return value from call(): 
+// if the worker fails to contact the coordinator, it can assume that the coordinator has exited because the job is done, and so the worker can terminate too. 
+// Depending on your design, you might also find it helpful to have a "please exit" pseudo-task that the coordinator can give to workers.
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
